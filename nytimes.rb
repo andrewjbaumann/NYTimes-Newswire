262b2befe41ae02753692417cd54f1b799c1fa3c
Andrew Baumann <baumann.andrew14@gmail.com>
2015-12-09 12:17:41 -0500

    added handling for newswire request

--- nytimes.rb
+++ nytimes.rb
@@ -5,4 +5,7 @@ require 'json'
 file = File.read('apiKeys.json')
 jsonHash = JSON.parse(file)
 
-puts jsonHash['books']
+wireURI = URI('http://api.nytimes.com/svc/news/v3/content/all/all.xml?limit=15&offset=50&api-key=' + jsonHash['newswire'])
+wireResponse = Net::HTTP.get(wireURI)
+wireResponseJson = JSON.parse(wireResponse)
+
